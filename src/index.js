ff9997013471443d826f30dd16c2b25d94392b9c
ChrisG <christophergarthright@gmail.com>
2020-03-04 16:41:09 -0500

    rendering ship breaks background loop

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import "./styles/index.scss";
-// import yodaStitch from "./images/yoda-stitch.jpg";
-const testObj = {
-  key1: "hi",
-  key2: {
-    key3: "Hello"
-  }
-};
+import Background from "./background"
+import Game from "./game"
+// import Player from "./player"
 
-const greeting = testObj?.key2?.key3 || testObj.key1;
-window.addEventListener("DOMContentLoaded", () => {
-  document.body.classList.add("center");
-  const card = document.createElement("div");
-  card.classList.add("card", "center");
-  card.innerHTML = `<h2>${greeting} World!</h2>`;
-  document.body.append(card);
-  const imgCard = document.createElement("div");
-  imgCard.classList.add("card", "center", "image-card");
-  document.body.appendChild(imgCard);
-});
+document.addEventListener("DOMContentLoaded", () => {
+  let x = new Background();
+  x.draw();
+  
+  new Game();
+
+})
